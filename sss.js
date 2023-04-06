c2c96764ef77539adb58de0e00f1aa860807aa27
shankarpandey1 <drspvet@gmail.com>
2023-04-06 13:45:10 -0400

    readme file added

---
+++ sss.js
@@ -0,0 +1,23 @@
+// if (true) {
+//     var x = 5;
+//     const y = 6;
+//     let z = 7;
+//   }
+//   console.log(x + y + z);
+
+
+	
+// console.log([3] == [3]);
+
+
+// console.log(3 == '3');
+
+
+// console.log(3 != '3');
+
+const lion = 1;
+let tiger = 2;
+var bear;4
+++lion;
+bear += lion + tiger;
+tiger++;
