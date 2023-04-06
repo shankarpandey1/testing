a1a901232b4296b4dd4ac63a61a70c40a83060b5
shankarpandey1 <drspvet@gmail.com>
2023-04-06 13:40:19 -0400

    first commit

---
+++ concate.js
@@ -0,0 +1,18 @@
+// let str1 = "Learning Javascript";
+// let str2 = "String";
+// let str3 = "Concatenation";
+// let final_str = `${str1} ${str2} ${str3} using Template Literals`;
+// console.log(final_str);
+
+
+let str1 = 'I\'ve you';
+let str2 = "more than";
+let str3 = "I can say,";
+let final_str = `${str1} ${str2} ${str3} is the nice song.`;
+console.log(final_str);
+
+let text1 = "Hello";
+let text2 = "world,";
+let text3 = "Have a nice day!";
+let result = text1.concat(" ", text2, " ", text3);
+console.log(result);
